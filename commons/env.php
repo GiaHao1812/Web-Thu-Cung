2923f839f03c12f817cb249942d8c604ebd42418
ninhnaph35348 <bombimquabombim12@gmail.com>
2024-09-24 10:04:12 +0700

    crudsanpham

--- commons/env.php
+++ commons/env.php
@@ -3,13 +3,13 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL', 'http://localhost/web-thu-cung/web-thu-cung/'); // Đường vào client
-define('BASE_URL_ADMIN', 'http://localhost/web-thu-cung/web-thu-cung/admin/'); // Đường vào admin
+define('BASE_URL', 'http://localhost/web-thu-cung/'); // Đường vào client
+define('BASE_URL_ADMIN', 'http://localhost/web-thu-cung/admin/'); // Đường vào admin
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'web_thu_cung');  // Tên database
+define('DB_NAME', 'web-thu-cung');  // Tên database
 
 define('PATH_ROOT', __DIR__ . '/../');
